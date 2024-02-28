a66dc6d82c4ab8381655e6567c6a4b79eafbac40
MariaFer1423 <fernanda.camacho@ulead.ac.cr>
2024-02-27 20:16:43 -0600

    Contador de numeros agregado

--- main.py
+++ main.py
@@ -6,3 +6,15 @@ def saludar_usuario():
 
 if __name__ == "__main__":
     saludar_usuario()
+
+
+def contador_numero():
+    numero = int(input("Ingresar un numero entero positivo: "))
+    if numero < 0:
+        print("Incorrecto, ingresar un numero entero positivo")
+        return
+    for i in range(1,numero + 1):
+        print(i)
+
+if __name__ == "__main__":
+    contador_numero()
