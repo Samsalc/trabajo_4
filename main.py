ef9dc3e94ac9fd19bc03e66b0008c8c530c76f2a
MariaFer1423 <fernanda.camacho@ulead.ac.cr>
2024-02-27 20:54:33 -0600

    Calculadora basica

--- main.py
+++ main.py
@@ -16,5 +16,54 @@ def contador_numero():
     for i in range(1,numero + 1):
         print(i)
 
+
 if __name__ == "__main__":
     contador_numero()
+
+
+
+def suma(x, y):
+    return x + y
+
+
+def multiplicar(x, y):
+    return x * y
+
+
+def division(x, y):
+    if y != 0:
+        return (x / y)
+
+    else:
+        print("No se puede dividir entro 0")
+        return None
+
+
+def resta(x, y):
+    return x - y
+
+
+opcion = input("Ingrese el numero de la opcion deseada (1 = suma, 2 = multiplicacion, 3 = division, 4 = resta): ")
+if opcion == '1':
+    numero_uno = float(input("Ingrese el primer numero: "))
+    numero_dos = float(input("Ingrese el segundo numero: "))
+    resultado = suma(numero_uno, numero_dos)
+    print(f"El resultado de la suma es: {resultado}")
+
+elif opcion == '2':
+    numero_uno = float(input("Ingrese el primer numero: "))
+    numero_dos = float(input("Ingrese el segundo numero: "))
+    resultado = multiplicar(numero_uno, numero_dos)
+    print(f"El resultado de la multiplicacion es: {resultado}")
+
+elif opcion == '3':
+    numero_uno = float(input("Ingrese el primer numero: "))
+    numero_dos = float(input("Ingrese el segundo numero: "))
+    resultado = division(numero_uno, numero_dos)
+    print(f"El resultado de la division es: {resultado}")
+
+else:
+    numero_uno = float(input("Ingrese el primer numero: "))
+    numero_dos = float(input("Ingrese el segundo numero: "))
+    resultado = resta(numero_uno, numero_dos)
+    print(f"El resultado de la resta es: {resultado}")
